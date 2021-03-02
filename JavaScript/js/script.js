9d8b68173903d14c21c43d0550325351e8a63f20
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-02 17:37:38 -0600

    finished L41

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,1 +1,17 @@
-var x = "Hello Planet Earth!"
+var message = "in global";
+console.log ("global: message = " + message);
+
+var a = function () {
+    var message = "inside a";
+    console.log ("a: message = " + message);
+
+    function b () {
+        console.log ("b: message = " + message);
+    }
+
+    b();
+
+}
+
+
+a();
