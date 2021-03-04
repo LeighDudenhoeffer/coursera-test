cb5602999afc2d8eccc19b1d168261e996cb8d9a
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-04 17:57:50 -0600

    finished L43 P1

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -4,8 +4,38 @@
 // console.log (string + "!");
 
 // ********** Regular math operators: +, -, *, /
-console.log ((5 * 4) / 3);
-console.log (undefined / 5);  //returns NaN (not a number)
+// console.log ((5 * 4) / 3);
+// console.log (undefined / 5);  //returns NaN (not a number)
+// function test1 (a) {
+//     console.log (a / 5);
+// }
+// test1();
+
+var x = '4', y = 4;
+// if (x == y) {
+//     console.log ("x=4 is equal to y=4");
+// }
+
+// x = "4"
+// if (x == y) {
+//     console.log ("x='4' is equal to y=4");
+// }
+
+// ***** Strict equality
+if (x === y) {
+    console.log ("Strict: x = '4' is equal to y = 4");
+}
+
+else {
+    console.log ("Strict: x = '4' is NOT equal to y = 4");
+}
+
+
+// ***** If statement (all false)
+// if (false || null ||)
+
+
+
 
 //Object Types
 //firstName: "Yaakov";
