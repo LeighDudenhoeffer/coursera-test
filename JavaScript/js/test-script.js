c06462b9a841f916fab082c9c9c1c0a3eaa5adb4
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-08 14:54:33 -0600

    textbook coding

--- JavaScript/js/test-script.js
+++ JavaScript/js/test-script.js
@@ -2,7 +2,7 @@ var friends = ['Jack', 'Jill', 'Judith', 'Terrence']
 
 alert (friends [3])
 
-
+// If/else statements
 if (1 != 2) {
     alert ("If you're not seeing this, we have bigger problems than JavaScript.");
     // 1 is never equal to 2, so we should always see this alert.
@@ -18,4 +18,9 @@ if ( test != "testing") {
     alert ("You haven't change anything."); // == and = will return this message
 } else {
     alert ("You've changed something!") // != will return this message
+}
+
+// Loops! A technique of DEVASTATING POWER!!!
+for (var i = 0; i <= 5; i++) {
+    alert (i); // This loop will trigger 6 alerts, reading 0, 1, 2, 3, 4, and 5 respectively.
 }
