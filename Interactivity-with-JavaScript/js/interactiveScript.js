1d113b3fa8875ee3b9ac9ca7aa6aee7787da2942
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-09 16:56:01 -0600

    js class code

--- Interactivity-with-JavaScript/js/interactiveScript.js
+++ Interactivity-with-JavaScript/js/interactiveScript.js
@@ -1,10 +1,7 @@
-var foo = ["Thank you for looking at my page! ", " five ", 5];
+// var foo = ["Thank you for looking at my page! ", " five ", 5];
 
-alert(foo [0]);
-alert(foo [1]);
-alert(foo [2]);
+// alert(foo [0]);
+// alert(foo [1]);
+// alert(foo [2]);
 
-alert (5 == 5);
-alert (5 != 7);
-
-alert ("5" === 5);
+alert ("Welcome to Interactivity with JavaScript!!");
