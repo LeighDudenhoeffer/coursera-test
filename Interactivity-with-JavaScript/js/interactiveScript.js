b56983dbe327a6c050e0f44c43473814404e7576
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-11 16:24:00 -0600

    completed week 1 of interactive jS

--- Interactivity-with-JavaScript/js/interactiveScript.js
+++ Interactivity-with-JavaScript/js/interactiveScript.js
@@ -4,8 +4,18 @@
 // alert(foo [1]);
 // alert(foo [2]);
 
-// alert ("Welcome to Interactivity with JavaScript!!");
-// prompt ("Enter your first name:")
-document.write ("Welcome to Interactivity with JavaScript!!") // not the best way to use this!
+alert ("Welcome to Interactivity with JavaScript!!");
+
+// document.write ("Welcome to Interactivity with JavaScript!!") // not the best way to use this!
 
 console.log ("document.write isn't the best thing to use and is considered a violation of code rules!")
+
+var name;
+    name = prompt ("Enter your name: ")
+    document.write (name);
+	document.write ("<h1>" + name + "</h1>");
+    document.write (name + 7+ "<br>");
+    document.write (document.title +"<br>");
+    var pars = document.getElementsByTagName ('p'); //this tells me how many paragraph elements exist on this page
+    document.write (pars + "</br>");
+    document.write (pars.length);
