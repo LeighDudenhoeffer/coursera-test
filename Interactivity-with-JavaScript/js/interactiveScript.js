92658c0cae151f57ebf0895a61ac57145567cd09
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-15 19:44:25 -0600

    created an interactive button for W02-events

--- Interactivity-with-JavaScript/js/interactiveScript.js
+++ Interactivity-with-JavaScript/js/interactiveScript.js
@@ -4,11 +4,10 @@
 
 console.log ("document.write isn't the best thing to use and is considered a violation of code rules!")
 
-// var name;
-//     name = prompt ("Enter your name: ")
+// function setDate() {
+//     document.getElementById('date').innerHTML=Date();
+// }
 
-// alert ("Greetings, " + name + "! Welcome to JavaScript!");
-
-function setDate() {
-    document.getElementById('date').innerHTML=Date();
+function displayDate() {
+    document.getElementById("demo").innerHTML=Date();
 }
