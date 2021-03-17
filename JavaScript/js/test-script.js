dba53156107946846fd3ccbb960417718645af25
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-17 15:49:56 -0600

    deleted old code

--- JavaScript/js/test-script.js
+++ JavaScript/js/test-script.js
@@ -2,17 +2,6 @@ var friends = ['Jack', 'Jill', 'Judith', 'Terrence']
 
 alert (friends [2])
 
-// If/else statements
-// if (1 != 2) {
-//     alert ("If you're not seeing this, we have bigger problems than JavaScript.");
-//     // 1 is never equal to 2, so we should always see this alert.
-// }
-
-// if (1 == 2) {
-//     alert ("If you're seeing this, we have bigger problems than JavaScript!");
-//     // 1 is NOT equal to 2, so this code will never run.
-// }
-
 
 var test = "testing";
 if ( test != "testing") {
@@ -20,11 +9,6 @@ if ( test != "testing") {
 } else {
     alert ("You've changed something!") // != will return this message
 }
-
-// Loops! A technique of DEVASTATING POWER!!!
-// for (var i = 0; i <= 0; i++) {
-//     alert (i); // This loop will trigger 6 alerts, reading 0, 1, 2, 3, 4, and 5 respectively.
-// }
 
 var myVariable = 7
 if (myVariable > 5) {
