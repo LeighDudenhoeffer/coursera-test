d7d7a3e93d30237f70baee008fce2f474a2370e5
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-30 16:38:33 -0600

    finished coding for IwJS wk2 assignment

--- Interactivity-with-JavaScript/js/gallery.js
+++ Interactivity-with-JavaScript/js/gallery.js
@@ -5,24 +5,7 @@ function upDate(previewPic){
     document.getElementById('image').innerHTML = alt;
 }
 
-
-    /* In this function you should
-    1) change the url for the background image of the div with the id = "image"
-    to the source file of the preview image
-
-    2) Change the text  of the div with the id = "image"
-    to the alt text of the preview image
-    */
-
-
-
-    function unDo(){
-    /* In this function you should
-    1) Update the url for the background image of the div with the id = "image"
-    back to the orginal-image.  You can use the css code to see what that original URL was
-
-    2) Change the text  of the div with the id = "image"
-    back to the original text.  You can use the html code to see what that original text was
-    */
-
+function unDo(){
+    document.getElementById('image').style.backgroundImage = "none";
+    document.getElementById('image').innerHTML = "Hover over a puppy below to display here.";
     }
