986dacbcf5cb51c6ff1bddb476a25311a667bd9c
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-30 16:21:33 -0600

    still working on class assignment code

--- Interactivity-with-JavaScript/js/gallery.js
+++ Interactivity-with-JavaScript/js/gallery.js
@@ -1,4 +1,8 @@
 function upDate(previewPic){
+
+}
+
+
     /* In this function you should
     1) change the url for the background image of the div with the id = "image"
     to the source file of the preview image
@@ -7,7 +11,7 @@ function upDate(previewPic){
     to the alt text of the preview image
     */
 
-    }
+
 
     function unDo(){
     /* In this function you should
