30b0e0108c71575a3a554f50db38df9f52e18ae4
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-04-20 17:45:13 -0600

    added more definition and another function

--- WebCoding/js/webbasics.js
+++ WebCoding/js/webbasics.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("paragraph").innerHTML = "A Paragraph Changed.";
 }
+
+function aFunction() {
+    document.getElementById("demo1").innerHTML="Hello Leigh!";
+    document.getElementById("demo2").innerHTML="How are you today?"
+}
