f9742fbdc39f3b960ea6bef954bc3d974a45a251
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-04-20 20:07:20 -0600

    added an anonymous function!

--- WebCoding/js/webbasics.js
+++ WebCoding/js/webbasics.js
@@ -1,13 +1,19 @@
+// This function only needs to be called once and doesn't need a name.
+//Clicking Hello (at the top of the page) turns it on and clicking the resulting, "Goodbye"
+//turns it off again.
+function sayGoodbye() {
+    $("#title").html("Goodbye")
+    $("#title").click(function() {
+        $("#title").html("Hello!");
+        $("#title").off("click");
+    });
+    };
+
 function myFunction() {
     document.getElementById("paragraph").innerHTML = "A Paragraph Changed.";
-}
+};
 
 function aFunction() {
     document.getElementById("demo1").innerHTML="Hello!!!";
     document.getElementById("demo2").innerHTML="How are you today?"
-}
-
-$("#title").click(function() {
-    $("#title").html("Hello!");
-    $("#title").off("click");
-});
+};
