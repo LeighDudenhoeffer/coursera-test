f4325a2e89a582873f2303581b6b72cbfdae8b8d
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-04-21 17:26:11 -0600

    holy crap this is difficult

--- WebCoding/js/webbasics.js
+++ WebCoding/js/webbasics.js
@@ -17,3 +17,15 @@ function aFunction() {
     document.getElementById("demo1").innerHTML="Hello!!!";
     document.getElementById("demo2").innerHTML="How are you today?"
 };
+
+function upDate(previewPic){
+    var src = previewPic.getAttribute("src");
+    var alt = previewPic.getAttribute("alt");
+    document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+    document.getElementById('image').innerHTML = alt;
+}
+
+function unDo(){
+    document.getElementById('image').style.backgroundImage = "none";
+    document.getElementById('image').innerHTML = "Hover over a puppy below to display here.";
+    }
