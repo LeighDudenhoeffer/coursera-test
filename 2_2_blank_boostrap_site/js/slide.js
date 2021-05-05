1327ecdd2f077a0f00a0c68782e310cee020e68e
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-05-04 18:03:20 -0600

    changed the background for what is mine and for final class score

---
+++ 2_2_blank_boostrap_site/js/slide.js
@@ -0,0 +1,19 @@
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideshow);
+
+function initSlideshow(slideshow) {
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+    var index = 0, time = 3000;
+    slides[index].classList.add('active');
+
+    setInterval(  () => {
+        slides[index].classList.remove('active');
+
+        index++;
+        if (index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
+
+    }, time);
+}
