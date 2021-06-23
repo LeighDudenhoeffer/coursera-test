4eae93e6246a2b9b6e8c9e40a716519ceae90ad4
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-23 17:44:28 -0600

    finished for the day

--- JavaScript/js/test-script.js
+++ JavaScript/js/test-script.js
@@ -1,24 +1,14 @@
-var friends = ['Good afternoon, Leigh', 'I am happy for you!', 'Hey, have a peach!', 'Greetings, Miss Pepper']
+var friends = ['Good afternoon, Leigh', 'I am happy for you!', 'Hey, have a peach!', 'Kiss Miss Pepper']
 
 alert (friends [0]);
 
 document.getElementById("demo").innerHTML = "This is not my First JavaScript!"
 
-// var happy = true;
-// var sad = false;
+var happy = true;
+var sad = false;
 
-// if (happy) {
-//     alert (friends [1]);
-// } else {
-//     alert (friends [2]);
-// }
-
-var red = true;
-var blue = false;
-
-if ( red ) {
-    document.getElementById("red").innerHtml = "This is true!"
-}
-else {
-    document.getElementById("blue").innerHTML = "This is false!"
+if (happy) {
+    alert (friends [1]);
+} else {
+    alert (friends [2]);
 }
