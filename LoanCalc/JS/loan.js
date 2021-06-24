6bb91e1893c665e8887e5fb2010fa2b4fbebb561
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-24 15:05:38 -0600

    working on JS project code

--- LoanCalc/JS/loan.js
+++ LoanCalc/JS/loan.js
@@ -6,5 +6,18 @@
 
 function calculate(){  //Look up the input and output elements in the document.
     var amount = document.getElementById("amount");
+    var apr = document.getElementById("apr");
+    var years = document.getElementById("years");
+    var zipcode = document.getElementById("years");
+    var payment = document.getElementById("payment");
+    var total = document.getElementById("total");
+    var totalinterest = document.getElementById("totalinterest");
+
+    // Get the user's input from the input elements. Assume all is valid. Convert interest from a
+    // percentage to a decimal, and convert from annual rate to monthly rate. Convert payment period in
+    // years to the total number of monthly payments.
+    var principal = parseFloat(amount.value);
+    var interest = parseFloat(apr.value) /100 / 12;
+    var payments = parseFloat(years.value) * 12;
 
 }
