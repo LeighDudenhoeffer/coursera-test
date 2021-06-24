5245fc2011eef5193462862bb0ec27736743780d
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-24 15:26:09 -0600

    I think I'm missing something in the JS catch statement...

--- LoanCalc/JS/loan.js
+++ LoanCalc/JS/loan.js
@@ -12,7 +12,6 @@ function calculate(){  //Look up the input and output elements in the document.
     var payment = document.getElementById("payment");
     var total = document.getElementById("total");
     var totalinterest = document.getElementById("totalinterest");
-
     // Get the user's input from the input elements. Assume all is valid. Convert interest from a
     // percentage to a decimal, and convert from annual rate to monthly rate. Convert payment period in
     // years to the total number of monthly payments.
@@ -23,13 +22,19 @@ function calculate(){  //Look up the input and output elements in the document.
     var x = Math.pow(1 + interest, payments);
     var monthly = (principal *x* interest)/(x-1);
 
+
     // If the result is a finite number, the input data was good and can display meaningful results.
     if (isFinite(monthly)) {  //This will fill in the output fields and round to 2 decimal places.
         payment.innerHTML = monthly.toFixed(2);
         total.innerHTML = (monthly * payments).toFixed(2);
 
-        //Save user input so it can be restored when they visit the loan calculater again.
+        // Save user input so it can be restored when they visit the loan calculater again.
         save(amount.value, apr.value, years.value, zipcode.value);
 
+        // Advertise: find and display local area lenders, but ignore network error trys.
+        {   // Catch any errors that occur within the curly braces
+            getLenders(amount.value, apr.value, years.value, zipcode.value);
+        }
+        catch(e) {}
     }
 }
