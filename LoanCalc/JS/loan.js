daa39f8c728efc5f520a0d5507a24091e2496341
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-24 15:01:30 -0600

    working on JS project code

--- LoanCalc/JS/loan.js
+++ LoanCalc/JS/loan.js
@@ -1,1 +1,10 @@
-// This loan app is uses strict ECMA 5
+// This loan app is uses strict ECMA 5 mode.
+
+// This script will define the calculate function that will be called by event handlers in the HTML code.
+// The function coded here will read values from <input> elements and calculate loan payment info and
+// display the info in the elements. It will also save the data, display links to lenders, and draw charts.
+
+function calculate(){  //Look up the input and output elements in the document.
+    var amount = document.getElementById("amount");
+
+}
