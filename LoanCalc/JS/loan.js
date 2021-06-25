e0b41f647e1045d7b50263a06a06a35d41b7aa22
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-24 20:55:16 -0600

    ok

--- LoanCalc/JS/loan.js
+++ LoanCalc/JS/loan.js
@@ -28,9 +28,6 @@ function calculate(){  //Look up the input and output elements in the document.
         total.innerHTML = (monthly * payments).toFixed(2);
         totalinterest.innerHTML = ((monthly * payments) - principal).toFixed(2);
 
-        // Save user input so it can be restored when they visit the loan calculater again.
-        // save(amount.value, apr.value, years.value, zipcode.value);
-
         // Advertise: find and display local area lenders, but ignore network error trys.
         try {   // Catch any errors that occur within the curly braces
             getLenders(amount.value, apr.value, years.value, zipcode.value);
@@ -46,31 +43,10 @@ function calculate(){  //Look up the input and output elements in the document.
         }
     }
 
-// Now save the user input as properties of the localStorage object. The storage feature may not work
-// in some browsers. However, it does work over HTTP.
-// function save(amount, apr, years, zipcode) {
-//     if (window.localStorage) {  // Do this only if the browser supports it.
-//         localStorage.loan_amount = amount;
-//         localStorage.loan_apr = apr;
-//         localStorage.loan_years = years;
-//         localStorage.loan_zipcode = zipcode;
-//     }
-// }
-
-// Attempt to restore the input fields when the document first loads.
-// window.onload = function() {
-//     // this will occur only if the browser supports local storage
-//     if (window.localStorage && localStorage.loan_amount) {
-//         document.getElementById(amount).value = localStorage.loan_amount;
-//         document.getElementById(apr).value = localStorage.loan_apr;
-//         document.getElementById(years).value = localStorage.loan_years;
-//         document.getElementById(zipcode).value = localStorage.loan_zipcode;
-//     }
-// };
-
 // At this point it works fine. I'll rem out the graph until I can get it figured right.
 
 // Pass the user input to a server-side script which will return a list of links to local lenders (zip code based).
+// This part only works in theory. I'll have to learn how to do it for modernity.
 function getLenders(amount, apr, years, zipcode) {
     // If the browser doesn't support the XMLHttpREquest object, do nothing
     if (!window.XMLHttpRequest) return;
