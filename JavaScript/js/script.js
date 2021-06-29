98a2f6356b7302b5003fcf2420a2bbcab4062c00
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-29 17:22:12 -0600

    working on JS loops

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -34,26 +34,3 @@ else {
 // ***** If statement (all false)
 // if (false || null ||)
 
-
-
-
-//Object Types
-//firstName: "Yaakov";
-//lastName:  "Chaikin";
-//social: {
-//    linkedin: "yaakovchaikin";
-//    twitter:  "yaakovchaikin";
-//    facebook: "CourseraWebDev";
-//}
-
-//Primitive Types: represents a single, immutable value
-    //Primitive Type: Undefined can only have one value (undefined)
-    //and you should never set a variable to undefined!
-    //Primitive Type: Null - lack of definition, can only have one value (null), can set a variable to null
-    //Primitive Type: Number - the only numeric type in JS - no integers
-    ///Primitive Type: String - is a sequence of characters used to represent text and
-    //can use single or double quotes to define ('text' or "text")
-    ///Primitive Type: Symbol -
-//not an object; once it's set, it can't be changed; value becomes read only
-//Boolean can only have 2 values: true or false
-
