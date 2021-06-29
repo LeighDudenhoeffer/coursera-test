93f2589837e19c1cee9304e4c5fa048b80f199f4
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-28 20:58:08 -0600

    this is neat

--- JavaScript/js/test-script.js
+++ JavaScript/js/test-script.js
@@ -13,18 +13,34 @@ if (happy) {
     alert (friends [2]);
 }
 
-var colors = ['red', 'green', 'blue', 'yellow'];
-var selectedColor = colors[1];
+var colors = ['red', 'green', 'blue', 'yellow', 'chartreuse', 'violet'];
+var selectedColor = colors[5];
 
-if( selectedColor == 'red') {
-    alert ("The color is red!");
-}
-else if ( selectedColor == 'green' ) {
-    alert ("The color is green!");
-}
-else if ( selectedColor == 'blue' ) {
-    alert ("The color is blue!");
-}
-else {
-    alert ("The color is yellow!");
+// if( selectedColor == 'red') {
+//     alert ("The color is red!");
+// }
+// else if ( selectedColor == 'green' ) {
+//     alert ("The color is green!");
+// }
+// else if ( selectedColor == 'blue' ) {
+//     alert ("The color is blue!");
+// }
+// else {
+//     alert ("The color is yellow!");
+// }
+
+switch (selectedColor) {
+    case "red": alert("The color is red!");
+        break;
+    case "blue": alert("The color is blue!");
+        break;
+    case "green": alert("The color is green!");
+        break;
+    case "yellow": alert("The color is yellow!");
+        break;
+    case "chartreuse": alert("The color is chartreuse!");
+        break;
+    case "violet": alert("The color is violet!");
+        break;
+    default: alert("There was an error.")
 }
