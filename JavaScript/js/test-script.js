51ecb7db2997fe989efad5c32f61641aba59a58d
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-06-28 20:50:54 -0600

    if, if else, else statements!

--- JavaScript/js/test-script.js
+++ JavaScript/js/test-script.js
@@ -7,8 +7,32 @@ document.getElementById("demo").innerHTML = "This is not my First JavaScript!"
 var happy = true;
 var sad = false;
 
-if (happy) {
+if (sad) {
     alert (friends [1]);
 } else {
     alert (friends [2]);
+}
+
+var red = true;
+var blue = false;
+var green;
+var myNum = 5;
+var otherNum = 7;
+var notNum = "chicken";
+var myValue = notNum - myNum;
+
+var colors = ['red', 'green', 'blue', 'yellow'];
+var selectedColor = colors[1];
+
+if( selectedColor == 'red') {
+    alert ("The color is red!");
+}
+else if ( selectedColor == 'green' ) {
+    alert ("The color is green!");
+}
+else if ( selectedColor == 'blue' ) {
+    alert ("The color is blue!");
+}
+else {
+    alert ("The color is yellow!");
 }
