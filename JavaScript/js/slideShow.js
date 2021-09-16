2be693b1488a42cb1094f9d88e7883d95ae28588
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-09-16 17:00:00 -0600

    edited all of the slides so they're the same size

--- JavaScript/js/slideShow.js
+++ JavaScript/js/slideShow.js
@@ -3,7 +3,7 @@
 
 (function(){
     "use strict";
-    const myImages = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image5.jpg"];
+    const myImages = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image5.jpg", "images/image6.jpg", "images/image7.jpg", "images/image8.jpg", "images/image9.jpg", "images/image10.jpg", "images/image11.jpg", "images/image12.jpg", "images/image13.jpg", "images/image14.jpg"];
     let currentImage = 0;
 
     document.getElementById('next').onclick = nextPhoto;
