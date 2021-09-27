6afae38753ac92b74e6953dc2113f063c4f92d87
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-09-27 17:30:24 -0500

    added code to get the form values from the forms when they're filled out

--- JavaScript/js/vacay.js
+++ JavaScript/js/vacay.js
@@ -4,4 +4,10 @@ detailsForm.addEventListener("submit", handleFormSubmit);
 
     function handleFormSubmit(event) {
         event.preventDefault();
+
+        var destName = event.target.elements["name"].value;
+        var destLocation = event.target.elements["location"].value;
+        var destPhoto = event.target.elements["photo"].value;
+        var destDesc = event.target.elements["description"].value;
     }
+
