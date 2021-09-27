acb1831434276a4736626dbe0a4274b5c8680d32
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-09-27 17:46:25 -0500

    more code and renamed my title and p id's so they're all on the same page as my brain

--- JavaScript/js/vacay.js
+++ JavaScript/js/vacay.js
@@ -2,15 +2,21 @@ var detailsForm = document.querySelector("#destination_details_form");
 
 detailsForm.addEventListener("submit", handleFormSubmit);
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
+function handleFormSubmit(event) {
+    event.preventDefault();
 
-        var destName = event.target.elements["name"].value;
-        var destLocation = event.target.elements["location"].value;
-        var destPhoto = event.target.elements["photo"].value;
-        var destDesc = event.target.elements["description"].value;
-    }
+    var destName = event.target.elements["name"].value;
+    var destLocation = event.target.elements["location"].value;
+    var destPhoto = event.target.elements["photo"].value;
+    var destDesc = event.target.elements["description"].value;
+
 
     for (var i = 0; i < detailsForm.length; i++) {
-
+        detailsForm.element[i].value = "";
     }
+
+    // create card here....
+
+    var wishListContainer = document.getElementById('title');
+
+}
