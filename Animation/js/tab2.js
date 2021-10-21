2dd2a6186dd858e5baf30d9ff1321908814c4e80
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-10-21 18:15:45 -0500

    still working on this. glad I did the css file first!

--- Animation/js/tab2.js
+++ Animation/js/tab2.js
@@ -1,14 +1,24 @@
-(function () {
+
     "use strict";
 
-    const tabs = document.querySelectorAll('#tabs > ul > li > a');
+    var tabs = document.querySelectorAll('#tabs > ul > li > a');
 
     for (var i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener('click', selectTab);
     }
 
     function selectTab(event) {
+        event.preventDefault();
 
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].removeAttribute('class');
+        }
+
+        event.target.className = 'active';
+
+        var thisTab = event.target.getAttribute('href');
+        var thisContent = document.querySelector(thisTab);
+
+        var oldTabContent = document.querySelector('.visible');
+        oldTabContent.className = 'visuallyhidden';
     }
-
-}());
