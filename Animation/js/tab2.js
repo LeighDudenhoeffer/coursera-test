08ba08edf4a9c4753309b8ec28dc87b4c924e05a
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-10-21 18:09:48 -0500

    continued adding js . Having issues with getting this to work like it did in jquery

--- Animation/js/tab2.js
+++ Animation/js/tab2.js
@@ -1,7 +1,14 @@
 (function () {
     "use strict";
 
+    const tabs = document.querySelectorAll('#tabs > ul > li > a');
 
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', selectTab);
+    }
 
+    function selectTab(event) {
+
+    }
 
 }());
