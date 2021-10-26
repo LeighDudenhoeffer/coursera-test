7619a123d26fea26ab3cb165ac507ebb09a4774b
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-10-26 18:56:42 -0500

    am adding the jQuery code to make the slider operate properly

--- Animation/js/slider.js
+++ Animation/js/slider.js
@@ -1,5 +1,21 @@
-const imageCount = $("#slider ul li").length;
-const imageWidth = $("#slider ul li img").first() .width();
+$(window).on('load', function(){
+    "use strict";
 
-alert(imageCount);
-alert(imageWidth);
+    const imageCount = $("#slider ul li").length;
+    const imageWidth = $("#slider ul li img").first() .width();
+    // const totalWidth = (imageCount * imageWidth) + "px";
+
+    // alert(totalWidth);
+
+    let leftPosition = 0;
+    let counter = 0;
+
+    $("#slider ul").css("width", totalWidth);
+
+    $("#next").click(function(){
+        counter++;
+        leftPosition = `-${counter * imageWidth}px`;
+        $("slider ul").animate( {left:leftPosition}, 700, "easeInQuad");
+    });
+
+});
