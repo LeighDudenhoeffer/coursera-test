5861f9e0901ae6d10b88858a5d538debb1e6f16f
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-10-26 19:39:51 -0500

    this sucks ass. I did this better with this jquery crap!

--- Animation/js/slider.js
+++ Animation/js/slider.js
@@ -4,24 +4,29 @@ $(window).on('load', function(){
     const imageCount = $("#slider ul li").length;
     const imageWidth = $("#slider ul li img").first().width();
     const totalWidth = (imageCount * imageWidth) + "px";
-
     // alert(totalWidth);
 
     let leftPosition = 0;
     let counter = 0;
 
-    $("#slider ul").css("width", totalWidth);
+    $('#slider ul').css("width", totalWidth);
 
-//     $("#next").click(function(){
-//         counter++;
+    $("#next").click(function(){
+
+        counter++;
+
+        leftPosition = `-${counter * imageWidth}+px`;
+
+        $("#slider ul").animate({ left: leftPosition }, 700, "easeInQuad");
+    });
 
 //         if(counter == imageCount){
 //             counter = 0;
 //         }
 
-//         leftPosition = `-${counter * imageWidth}px`;
-//         $("slider ul").animate( {left:leftPosition}, 700, "easeInQuad");
-//     });
+//
+//
+//
 
 //     $("#previous").click(function(){
 
