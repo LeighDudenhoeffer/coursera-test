b4330c77bf977168b2e47f886c909d34243087af
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-10-29 14:02:47 -0500

    working on the js for theclass code

--- Animation/js/JS_slideshow.js
+++ Animation/js/JS_slideshow.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
     // How many slides?
     const slideCount = document.querySelectorAll('#slider-wrapper ul li').length;
     // How wide are the slides?
+    // alert(slideCount);
     const slideWidth = document.querySelector('#slider-wrapper').offsetWidth;
     // Total slider width
     const totalWidth = slideCount * slideWidth + 'px';
@@ -11,4 +12,13 @@ window.addEventListener('load', function(){
     const next = document.getElementById('next');
     // Previous button
     const previous = document.getElementById('previous');
+
+    // Upper left corner of the slider
+    let leftPosition = 0;
+    // slide counter
+    let counter = 0;
+    // Set the width of the slider (also in the css)
+    slider.style.width = totalWidth;
+
+
 });
