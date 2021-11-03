956830534f27232512aef5625a5abaa866ac70a3
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-03 12:06:16 -0500

    worked on the js on my own. it's not working, but I'll learn why

--- Animation/js/timedslider.js
+++ Animation/js/timedslider.js
@@ -2,7 +2,7 @@ $(window).on('load', function(){
     "use strict";
 
     const imageCount = $("#slider ul li").length;
-    const imageWidth = $("#slider ul li img").first().width();
+    const imageWidth = $("#slider ul li: first img").width();
     const totalWidth = (imageCount * imageWidth) + "px";
 
     let leftPosition = 0;
@@ -10,25 +10,25 @@ $(window).on('load', function(){
 
     $('#slider ul').css("width", totalWidth);
 
-    counter++;
 
-    if(counter === imageCount){
+    setInterval(slider, 3000);
 
-        $("#slider ul").clone().appendTo("#slider");
-        $("#slider ul").last().css("left", imageWidth + "px");
+    function slider(){
+        counter++;
 
-        leftPosition = `-${totalWidth}`;
+        if (counter === imageCount) {
+            counter = 0;}
 
-        $("#slider ul").last().animate({left: 0}, 500, "easeInQuad");
-        $("#slider ul").first().animate({left:leftPosition}, 500, "easeInQuad", function(){
-            $("#slider ul").first().remove();
-        });
+            leftPosition = `-${counter * imageWidth}px`;
 
-        counter = 0;
+            $("#slider ul").animate(
+                { left: leftPosition }, 500, "easeInQuad");
 
-    } else {
-        leftPosition = `-${counter * imageWidth}px`;
-        $("#slider ul").animate({left:leftPosition}, 500, "easeInQuad");
-    }
+    };
 
 });
+
+let mySlider = setInterval(slider, 3000);
+
+$("#slider").mouseover(function(){ clearInterval(mySlider) });
+$("#slider").mouseout(function(){ mySlider = setInterval(slider, 3000); });
