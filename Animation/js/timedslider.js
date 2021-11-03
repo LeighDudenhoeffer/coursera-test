c974b49ea8aa6e4472128370712f29c9e173a833
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-03 16:06:12 -0500

    finished the js code for class. It works. Now, I want to add instructions for guest users so they know to hover their mouse to stop the slideshow and move the mouse out of the box to restart the slideshow

--- Animation/js/timedslider.js
+++ Animation/js/timedslider.js
@@ -10,7 +10,9 @@ $(window).on('load', function(){
 
     $('#slider ul').css("width", totalWidth);
 
-    setInterval(function(){
+    let mySlider = setInterval( slider, 2500 );
+
+    function slider(){
         counter++;
 
         if (counter === imageCount) {
@@ -21,7 +23,7 @@ $(window).on('load', function(){
             leftPosition = `-${totalWidth}`;
 
             $("#slider ul").last().animate({ left: 0 }, 500, "easeInQuad");
-            $("#slider ul").first().animate({ left: leftPosition }, 500, "easeInQuad", function(){
+            $("#slider ul").first().animate({ left: leftPosition }, 500, "easeInQuad", function () {
                 $("#slider ul").first().remove();
             });
 
@@ -32,11 +34,14 @@ $(window).on('load', function(){
             leftPosition = `-${counter * imageWidth}px`;
             $("#slider ul").animate({ left: leftPosition }, 500, "easeInQuad");
         }
-    }, 2500 );
+    }
 
-let mySlider = setInterval(mySlider, 2500);
+    document.getElementById("slider").addEventListener("mouseover", function(){
+        clearInterval(mySlider);
+    });
 
-$("#slider").mouseover(function(){clearInterval(mySlider) });
-$("#slider").mouseout(function(){mySlider = setInterval(slider, 2500); });
+    document.getElementById("slider").addEventListener("mouseout", function () {
+        mySlider = setInterval(slider, 2500);
+    });
 
 });
