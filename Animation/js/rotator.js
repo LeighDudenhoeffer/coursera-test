f562f399f63ac3fcd08fc2b52f5712a00da4f903
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-04 14:18:31 -0500

    working on getting the js right

--- Animation/js/rotator.js
+++ Animation/js/rotator.js
@@ -4,7 +4,16 @@
     let counter = 1;
 
     function contentRotator(){
-        $(`#container p:nth-child(${counter})`).fadeIn(2000);
+        $(`#container p:nth-child(${counter})`).fadeIn(2000, function(){
+            if( $(this).is( "#container p:last-child") ){
+
+            }
+            else{
+                setTimeout(function(){
+                    $(`#container p:nth-child(${counter})`).fadeOut(2000, function(){});
+                }, 5000);
+            }
+        });
     }
 
     contentRotator();
