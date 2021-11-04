86204be67a8db65b0a55ac56dcdb96dd3153ad84
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-04 13:19:43 -0500

    I had the js file referenced incorrectly which was causing the page to fail; now we're doing the 2000 millisecond fadein

--- Animation/js/rotator.js
+++ Animation/js/rotator.js
@@ -4,9 +4,11 @@
     let counter = 1;
 
     function contentRotator(){
+        $(`#container p:nth-child(${counter})`).fadeIn(2000, function(){
 
+        });
     }
 
     contentRotator();
 
-})();
+}());
