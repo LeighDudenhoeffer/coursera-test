6d0b9d74ee6f8450a625587d9a0a3e572161a736
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-04 14:09:03 -0500

    went back to the beginning for js because I couldn't find where I was missing a ')'

--- Animation/js/rotator.js
+++ Animation/js/rotator.js
@@ -4,9 +4,7 @@
     let counter = 1;
 
     function contentRotator(){
-        $(`#container p:nth-child(${counter})`).fadeIn(2000, function(){
-
-        });
+        $(`#container p:nth-child(${counter})`).fadeIn(2000);
     }
 
     contentRotator();
