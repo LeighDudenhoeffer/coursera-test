1ae998fdee88017e7e3f746a2c82492de4b31d32
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-04 16:57:24 -0500

    The additional timeout in the if statement makes the rotator rotate non-stop!

--- Animation/js/rotator.js
+++ Animation/js/rotator.js
@@ -6,7 +6,12 @@
     function contentRotator(){
         $(`#container p:nth-child(${counter})`).fadeIn(2000, function(){
             if( $(this).is( "#container p:last-child") ){
-
+                setTimeout(function(){
+                    $(`#container p:nth-child(${counter})`).fadeOut(2000, function(){
+                        counter=1;
+                        contentRotator();
+                    });
+                }, 5000);
             }
             else{
                 setTimeout(function(){
