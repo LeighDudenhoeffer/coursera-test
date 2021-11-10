b104d595460e1b12cf6794b3e246ee7f00c7a675
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-10 15:32:47 -0600

    created new class files for L7 Flexslider advanced

---
+++ Animation/js/bumbles.js
@@ -0,0 +1,10 @@
+$(window).on('load', function(){
+    $('.flexslider').flexslider(
+        {
+            animation: "slide",
+            slideshowSpeed: 2750,
+            direction: "horizontal",
+            pauseOnHover: true
+        }
+    );
+});
