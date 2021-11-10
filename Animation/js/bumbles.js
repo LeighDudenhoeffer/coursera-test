8f17a9cd8c012d4cc9789a59384db25243ee4c1a
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-10 16:10:11 -0600

    finished css code and working on js code for advanced flexslider

--- Animation/js/bumbles.js
+++ Animation/js/bumbles.js
@@ -2,9 +2,18 @@ $(window).on('load', function(){
     $('.flexslider').flexslider(
         {
             animation: "slide",
-            slideshowSpeed: 2750,
+            slideshowSpeed: 3000,
             direction: "horizontal",
-            pauseOnHover: true
+            pauseOnHover: true,
+            before: function(){
+                $(".cta").css("bottom", "100%");
+            },
+            start: function(){
+                $(".cta").animate({bottom: "5%"}, 3000, "easeOutElastic");
+            },
+            after: function(){
+                $(".cta").animate({bottom: "5%"}, 3000, "easeOutElastic");
+            }
         }
     );
 });
