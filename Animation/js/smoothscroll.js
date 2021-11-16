68c6a943f1f2f65275c80a2e35c1c91523d368e2
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-16 17:15:09 -0600

    playing with the body and header areas

--- Animation/js/smoothscroll.js
+++ Animation/js/smoothscroll.js
@@ -2,6 +2,7 @@ $('nav ul li a').click(function(){
     var thisSection = $(this).attr('href');
 
     $('html, body').stop().animate({
+
         scrollTop: $(thisSection).offset().top
     }, 1800, "easeOutCirc");
 
