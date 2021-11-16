2389bdf8b8f88cc4e5a6d968c16bdf5e4ae66dcf
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-11-16 16:55:38 -0600

    for now I've added a TOP link to each section until I get this figured out

--- Animation/js/smoothscroll.js
+++ Animation/js/smoothscroll.js
@@ -1,5 +1,5 @@
 $('nav ul li a').click(function(){
     var thisSection = $(this).attr('href');
 
-    $('html, body').stop().animate({scrollTop: $(thisSection).offset().top}, 1800);
+    $('html, body').stop().animate({scrollTop: $(thisSection).offset().top}, 1800, "easeOutCirc");
 });
