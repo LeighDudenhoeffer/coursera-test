9184862aecbd471e9a3a97f93dbbb2d248bf7533
Mrs. D <69485246+LeighDudenhoeffer@users.noreply.github.com>
2025-01-21 15:16:44 -0600

    Delete Animation directory
    housekeeping

--- Animation/js/smoothscroll.js
+++
@@ -1,9 +0,0 @@
-$('nav ul li a').click(function(){
-    var thisSection = $(this).attr('href');
-
-    $('html, body').stop().animate({
-
-        scrollTop: $(thisSection).offset().top
-    }, 1800, "easeOutCirc");
-
-});
